448f2f8eca419545ae7c7dbf3b5cad0d929d9574
mizukami1234 <mizukami19960914@gmail.com>
2021-01-13 15:36:19 +0900

    新規投稿ページへの遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+
 
   def index
     @prototypes = Prototype.all
@@ -47,15 +47,17 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
-  private
-  def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
   def move_to_index
     prototype = Prototype.find(params[:id])
     unless current_user.id == prototype.user.id
       redirect_to action: :index
     end
   end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  
 end
